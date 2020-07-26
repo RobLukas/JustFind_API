b007fa3a7751d81e1205d3041eb116e2e2f67c14
Robert Łukasik <rolukasik@gmail.com>
2020-07-26 13:17:05 +0200

    Add dto's for create and update companies;

--- src/features/companies/dto/createCompany.dto.ts
+++ src/features/companies/dto/createCompany.dto.ts
@@ -1,3 +1,30 @@
-export default class CreateCompanyDto {
-    
+import {
+  MaxLength,
+  IsNotEmpty,
+  IsString,
+  IsNumberString,
+} from 'class-validator';
+
+class CreateCompanyDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(150)
+  logo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(150)
+  website: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  size: number;
 }
+
+export default CreateCompanyDto;
