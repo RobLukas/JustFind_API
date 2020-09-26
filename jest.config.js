7718b7011cb67385b2bd0c4a5d35b033c7815566
Robert Łukasik <rolukasik@gmail.com>
2020-09-27 01:29:53 +0200

    Create jest config for unit tests;

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+const { resolve } = require('path');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  clearMocks: true,
+  roots: ['<rootDir>'],
+  modulePaths: ['<rootDir>'],
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.json',
+    },
+  },
+  moduleNameMapper: {
+    '^app/(.*)$': resolve(__dirname, './src/app/$1'),
+    '^config/(.*)$': resolve(__dirname, './src/config/$1'),
+    '^utils/(.*)$': resolve(__dirname, './src/utils/$1'),
+    '^database/(.*)$': resolve(__dirname, './src/database/$1'),
+    '^features/(.*)$': resolve(__dirname, './src/features/$1'),
+    '^offers/(.*)$': resolve(__dirname, './src/features/offers/$1'),
+    '^companies/(.*)$': resolve(__dirname, './src/features/companies/$1'),
+    '^offices/(.*)$': resolve(__dirname, './src/features/offices/$1'),
+  },
+  verbose: true,
+};
