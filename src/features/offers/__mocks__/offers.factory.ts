2755bed71bacdfc630154b8f5f73ea396c9a35d1
Robert Łukasik <rolukasik@gmail.com>
2020-09-28 23:30:25 +0200

    Add offers factory;

---
+++ src/features/offers/__mocks__/offers.factory.ts
@@ -0,0 +1,37 @@
+import { each } from 'factory.ts';
+import * as faker from 'faker';
+
+import BasicEntityMockFactory from 'features/__mocks__/basicEntity.factory';
+import randomEnum from 'utils/getRandomEnum';
+import roundSalary from 'utils/roundSalary';
+import Offers from 'offers/interface/offers.interface';
+import { Currency } from 'offers/types/currency.types';
+import { ExperienceLevel } from 'offers/types/experienceLevel.types';
+import { MainTechnology } from 'offers/types/mainTechnology.types';
+import { SkillLevel } from 'offers/types/technologySkillLevel.types';
+import { makeFactoryWithRequired } from 'factory.ts/lib/sync';
+
+export const OffersMockFactory = makeFactoryWithRequired<Offers, 'companyId'>({
+  title: each(() => faker.name.jobTitle()),
+  slug: each(() =>
+    faker.helpers.slugify(
+      `${faker.company.companyName()} ${faker.name.jobTitle()}`,
+    ),
+  ),
+  salaryFrom: each(() => roundSalary(faker.random.number({ min: 1000 }))),
+  salaryTo: each(() => roundSalary(faker.random.number({ min: 1000 }))),
+  description: each(() => faker.name.jobDescriptor()),
+  technologies: [
+    {
+      technology: 'technology1',
+      skillLevel: randomEnum(SkillLevel),
+    },
+    {
+      technology: 'technology2',
+      skillLevel: randomEnum(SkillLevel),
+    },
+  ],
+  currency: randomEnum(Currency),
+  experienceLevel: randomEnum(ExperienceLevel),
+  mainTechnology: randomEnum(MainTechnology),
+}).combine(BasicEntityMockFactory);
